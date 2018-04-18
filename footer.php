db0f912d253e9b94b341458948f1653bff1bba62
Mahdi Yazdani <mahdiyazdani@mail.com>
2018-04-19 01:33:48 +0400

    Updated site info HTML markup

--- footer.php
+++ footer.php
@@ -34,10 +34,23 @@
 			 * Functions hooked in to mypreview_conj_lite_footer action
 			 *
 			 * @hooked mypreview_conj_lite_footer_widgets			 - 10
-			 * @hooked mypreview_conj_lite_credit        			 - 20
 			 */
 			do_action( 'mypreview_conj_lite_footer' ); ?>
 
+			<div class="site-info">
+
+				<?php
+				/* translators: 1: WordPress.org URL. */
+				printf( esc_html__( '%1$sProudly powered by %2$s ', 'conj-lite' ), '<span class="site-wp-credits">', '<a href="https://wordpress.org" target="_blank">WordPress</a></span>' );
+
+				printf( wp_kses_post( '%1$s%2$s%3$s' ), '<span>', apply_filters( 'mypreview_conj_lite_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date_i18n( __( 'Y', 'conj-lite' ) ) ), '</span>' );
+
+				/* translators: 1: Seperator, 2: Theme name, 3: Theme author. */
+				printf( esc_html__( '%1$s Theme: %2$s by %3$s', 'conj-lite' ), '<span><span class="sep"> | </span>', esc_html( MYPREVIEW_CONJ_LITE_THEME_NAME ), '<a href="' . esc_url( MYPREVIEW_CONJ_LITE_THEME_URI ) . '">' . esc_html( MYPREVIEW_CONJ_LITE_THEME_AUTHOR ) . '</a>.</span>' );
+
+				?>
+
+			</div><!-- .site-info -->
 		</div><!-- .col-full -->
 	</footer><!-- #colophon -->
 
