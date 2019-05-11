b07e30dcef4a94ec420f9aff873ee77d8572c4ef
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 18:29:41 +0400

    Added hooked methods

--- includes/woocommerce/conj-lite-woocommerce-template-functions.php
+++ includes/woocommerce/conj-lite-woocommerce-template-functions.php
@@ -2,21 +2,25 @@
 /**
  * WooCommerce template functions.
  *
- * @package 	mypreview-conj
+ * @requires 	WooCommerce
+ * @author  	Mahdi Yazdani
+ * @package 	conj-lite
+ * @since 	    1.1.0
  */
 
 /**
  * WooCommerce shop messages.
  * 
- * @uses    mypreview_conj_lite_do_shortcode
+ * @uses    is_checkout()
+ * @uses    conj_lite_do_shortcode()
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_shop_messages' ) ) :
-	function mypreview_conj_lite_wc_shop_messages() {
+if ( ! function_exists( 'conj_lite_wc_shop_messages' ) ) :
+	function conj_lite_wc_shop_messages() {
 
 		if ( ! is_checkout() ) {
-			echo wp_kses_post( mypreview_conj_lite_do_shortcode( 'woocommerce_messages' ) );
-		}
+			echo wp_kses_post( conj_lite_do_shortcode( 'woocommerce_messages' ) );
+		} // End If Statement
 
 	}
 endif;
@@ -27,8 +31,8 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_wrapper_before' ) ) :
-	function mypreview_conj_lite_wc_wrapper_before() {
+if ( ! function_exists( 'conj_lite_wc_wrapper_before' ) ) :
+	function conj_lite_wc_wrapper_before() {
 
 		?>
 		<div id="primary" class="content-area">
@@ -44,8 +48,8 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_wrapper_after' ) ) :
-	function mypreview_conj_lite_wc_wrapper_after() {
+if ( ! function_exists( 'conj_lite_wc_wrapper_after' ) ) :
+	function conj_lite_wc_wrapper_after() {
 
 		?>
 			</main><!-- #main -->
@@ -60,11 +64,10 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_sorting_wrapper' ) ) :
-	function mypreview_conj_lite_wc_sorting_wrapper() {
+if ( ! function_exists( 'conj_lite_wc_sorting_wrapper' ) ) :
+	function conj_lite_wc_sorting_wrapper() {
 
-		?><div class="conj-wc-sorting">
-		<?php
+		?><div class="conj-lite-wc-sorting"><?php
 
 	}
 endif;
@@ -75,15 +78,16 @@ endif;
  * but since this method adds pagination before that function is excuted we need a separate function to
  * determine whether or not to display the pagination.
  *
+ * @uses 	woocommerce_products_will_display();
  * @uses 	woocommerce_pagination();
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_pagination' ) ) :
-	function mypreview_conj_lite_wc_pagination() {
+if ( ! function_exists( 'conj_lite_wc_pagination' ) ) :
+	function conj_lite_wc_pagination() {
 
 		if ( woocommerce_products_will_display() ) {
 			woocommerce_pagination();
-		}
+		} // End If Statement
 
 	}
 endif;
@@ -93,10 +97,10 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_sorting_wrapper_close' ) ) :
-	function mypreview_conj_lite_wc_sorting_wrapper_close() {
+if ( ! function_exists( 'conj_lite_wc_sorting_wrapper_close' ) ) :
+	function conj_lite_wc_sorting_wrapper_close() {
 
-		?></div><!-- .conj-wc-sorting -->
+		?></div><!-- .conj-lite-wc-sorting -->
 		<?php
 
 	}
@@ -105,14 +109,16 @@ endif;
 /**
  * Product columns wrapper.
  *
- * @uses 	@static 	products_per_column()
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_product_cols_wrapper' ) ) :
-	function mypreview_conj_lite_wc_product_cols_wrapper() {
+if ( ! function_exists( 'conj_lite_wc_product_cols_wrapper' ) ) :
+	function conj_lite_wc_product_cols_wrapper() {
 
-		$columns = (int) MyPreview_Conj_Lite_WooCommerce::products_per_column( $number = null );
-		echo '<div class="conj-wc-product-columns archive-columns-' . absint( $columns ) . '">';
+		// Get the default columns setting
+		// this is how many products will be shown per row in loops.
+		$columns = (int) ( function_exists( 'wc_get_default_products_per_row' ) )  ?  wc_get_default_products_per_row()  :  2;
+
+		printf( '<div class="conj-lite-wc-product-columns archive-columns-%1$s">', intval( $columns ) );
 
 	}
 endif;
@@ -122,10 +128,10 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_product_cols_wrapper_close' ) ) :
-	function mypreview_conj_lite_wc_product_cols_wrapper_close() {
+if ( ! function_exists( 'conj_lite_wc_product_cols_wrapper_close' ) ) :
+	function conj_lite_wc_product_cols_wrapper_close() {
 
-		?></div><!-- .conj-wc-product-columns -->
+		?></div><!-- .conj-lite-wc-product-columns -->
 		<?php
 
 	}
@@ -138,10 +144,10 @@ endif;
  * @uses    woocommerce_upsell_display()
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_upsell_display' ) ) :
-	function mypreview_conj_lite_wc_upsell_display() {
+if ( ! function_exists( 'conj_lite_wc_upsell_display' ) ) :
+	function conj_lite_wc_upsell_display() {
 
-		$columns = apply_filters( 'mypreview_conj_lite_wc_upsells_columns', 3 );
+		$columns = apply_filters( 'conj_lite_wc_upsells_columns', 3 );
 		woocommerce_upsell_display( -1, $columns );
 
 	}
@@ -153,11 +159,10 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_product_flash_wrapper' ) ) :
-	function mypreview_conj_lite_wc_product_flash_wrapper() {
+if ( ! function_exists( 'conj_lite_wc_product_flash_wrapper' ) ) :
+	function conj_lite_wc_product_flash_wrapper() {
 
-		?><div class="conj-wc-product__flashs">
-		<?php
+		?><div class="conj-lite-wc-product__flashs"><?php
 
 	}
 endif;
@@ -168,38 +173,10 @@ endif;
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_product_flash_wrapper_close' ) ) :
-	function mypreview_conj_lite_wc_product_flash_wrapper_close() {
+if ( ! function_exists( 'conj_lite_wc_product_flash_wrapper_close' ) ) :
+	function conj_lite_wc_product_flash_wrapper_close() {
 
-		?></div><!-- .conj-wc-product__flash -->
-		<?php
-
-	}
-endif;
-
-/**
- * Display WooCommerce product search field.
- *
- * @see 	https://docs.woocommerce.com/document/woocommerce-product-search/api/#section-2
- * @see 	http://hookr.io/constants/icl_language_code/
- * @link 	https://woocommerce.com/products/woocommerce-product-search
- * @uses 	ICL_LANGUAGE_CODE;
- * @uses 	woocommerce_product_search();
- * @return  void
- */
-if ( ! function_exists( 'mypreview_conj_lite_wc_search_field' ) ) :
-	function mypreview_conj_lite_wc_search_field() {
-
-		?>
-		<div class="site-wc-search">
-			<?php
-			$instance = array(
-				'title' => ''
-			);
-
-            the_widget( 'WC_Widget_Product_Search', $instance );
-			?>
-		</div>
+		?></div><!-- .conj-lite-wc-product__flash -->
 		<?php
 
 	}
@@ -209,17 +186,19 @@ endif;
  * Cart Fragments.
  * Ensure cart contents update when products are added to the cart via AJAX.
  *
- * @uses 	mypreview_conj_lite_wc_cart_link
+ * @uses 	ob_start()
+ * @uses 	ob_get_clean()
+ * @uses 	conj_lite_wc_cart_link
  * @param  	array $fragments Fragments to refresh via AJAX.
  * @return 	array            Fragments to refresh via AJAX
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_cart_link_fragment' ) ) :
-	function mypreview_conj_lite_wc_cart_link_fragment( $fragments ) {
+if ( ! function_exists( 'conj_lite_wc_cart_link_fragment' ) ) :
+	function conj_lite_wc_cart_link_fragment( $fragments ) {
 
 		global $woocommerce;
 
 		ob_start();
-		mypreview_conj_lite_wc_cart_link();
+		conj_lite_wc_cart_link();
 		$fragments['a.cart-contents'] = ob_get_clean();
 
 		return $fragments;
@@ -228,13 +207,35 @@ if ( ! function_exists( 'mypreview_conj_lite_wc_cart_link_fragment' ) ) :
 endif;
 
 /**
+ * Display WooCommerce product search field.
+ *
+ * @see 	https://docs.woocommerce.com/document/woocommerce-product-search/api/#section-2
+ * @uses 	conj_lite_do_shortcode()
+ * @uses 	woocommerce_product_search();
+ * @return  void
+ */
+if ( ! function_exists( 'conj_lite_wc_search_field' ) ) :
+	function conj_lite_wc_search_field() {
+
+		?>
+		<div class="site-wc-search">
+			<?php the_widget( 'WC_Widget_Product_Search', 'title=' ); ?>
+		</div>
+		<?php
+
+	}
+endif;
+
+/**
  * Cart Link.
  * Displayed a link to the cart including the number of items present and the cart total.
  *
+ * @uses 	get_cart_subtotal()
+ * @uses 	get_cart_contents_count()
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_cart_link' ) ) :
-	function mypreview_conj_lite_wc_cart_link() {
+if ( ! function_exists( 'conj_lite_wc_cart_link' ) ) :
+	function conj_lite_wc_cart_link() {
 
 		?>
 		<a class="cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', 'conj-lite' ); ?>">
@@ -249,11 +250,13 @@ endif;
 /**
  * Display Header Cart.
  *
- * @uses 	mypreview_conj_lite_wc_cart_link();
+ * @uses 	is_cart()
+ * @uses 	the_widget()
+ * @uses 	conj_lite_wc_cart_link()
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_header_cart' ) ) :
-	function mypreview_conj_lite_wc_header_cart() {
+if ( ! function_exists( 'conj_lite_wc_header_cart' ) ) :
+	function conj_lite_wc_header_cart() {
 
 		if ( is_cart() ) {
 			$class = 'current-menu-item';
@@ -262,18 +265,8 @@ if ( ! function_exists( 'mypreview_conj_lite_wc_header_cart' ) ) :
 		} // End If Statement
 		?>
 		<ul id="site-header-cart" class="site-header-cart">
-			<li class="<?php echo esc_attr( $class ); ?>">
-				<?php mypreview_conj_lite_wc_cart_link(); ?>
-			</li>
-			<li>
-				<?php
-				$instance = array(
-					'title' => ''
-				);
-
-				the_widget( 'WC_Widget_Cart', $instance );
-				?>
-			</li>
+			<li class="<?php echo esc_attr( $class ); ?>"><?php conj_lite_wc_cart_link(); ?></li>
+			<li><?php the_widget( 'WC_Widget_Cart', 'title=' ); ?></li>
 		</ul>
 		<?php
 
@@ -283,37 +276,42 @@ endif;
 /**
  * Used to wrap the "#order_review" tag.
  *
+ * @uses 	get_theme_mod()
+ * @uses 	conj_is_not_checkout_page()
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_checkout_order_review_wrapper' ) ) :
-	function mypreview_conj_lite_wc_checkout_order_review_wrapper() {
-		
-		?><div class="conj-wc-checkout-order-review__wrapper">
-		<?php
+if ( ! function_exists( 'conj_lite_wc_checkout_order_review_wrapper' ) ) :
+	function conj_lite_wc_checkout_order_review_wrapper() {
+
+		?><div class="conj-lite-wc-checkout-order-review__wrapper"><?php
+
 	}
 endif;
+
 /**
  * Prepends a heading tag to the "#order_review" container.
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_checkout_order_review_heading' ) ) :
-	function mypreview_conj_lite_wc_checkout_order_review_heading() {
-		
-		?><h3 class="conj-wc-checkout-order-review__heading"><?php esc_html_e( 'Your order', 'conj-lite' ); ?></h3>
-		<?php
+if ( ! function_exists( 'conj_lite_wc_checkout_order_review_heading' ) ) :
+	function conj_lite_wc_checkout_order_review_heading() {
+
+		?><h3 class="conj-lite-wc-checkout-order-review__heading"><?php esc_html_e( 'Your order', 'conj-lite' ); ?></h3><?php
+
 	}
 endif;
+
 /**
  * Used to close the wrapper of "#order_review" tag.
  *
  * @return  void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_checkout_order_review_wrapper_close' ) ) :
-	function mypreview_conj_lite_wc_checkout_order_review_wrapper_close() {
-		
-		?></div><!-- .conj-wc-checkout-order-review__wrapper -->
+if ( ! function_exists( 'conj_lite_wc_checkout_order_review_wrapper_close' ) ) :
+	function conj_lite_wc_checkout_order_review_wrapper_close() {
+
+		?></div><!-- .conj-lite-wc-checkout-order-review__wrapper -->
 		<?php
+
 	}
 endif;
 
@@ -327,38 +325,35 @@ endif;
  * @param 	boolean $recent_fallback Should the function display recent products as a fallback when there are no featured or on-sale products?.
  * @uses  	wc_get_featured_product_ids()
  * @uses  	wc_get_product_ids_on_sale()
- * @uses  	mypreview_conj_lite_do_shortcode()
+ * @uses  	conj_lite_do_shortcode()
  * @return 	void
  */
-if ( ! function_exists( 'mypreview_conj_lite_wc_promoted_products' ) ) :
-	function mypreview_conj_lite_wc_promoted_products( $per_page = '4', $columns = '2', $recent_fallback = TRUE ) {
+if ( ! function_exists( 'conj_lite_wc_promoted_products' ) ) :
+	function conj_lite_wc_promoted_products( $per_page = '4', $columns = '2', $recent_fallback = TRUE ) {
 
 		if ( wc_get_featured_product_ids() ) {
-
 			/* translators: 1: Opening heading tag, 2: Closing heading tag. */
 			printf( wp_kses_post( '%1$sFeatured Products%2$s', 'conj-lite' ), '<h2>', '</h2>' );
 
-			echo wp_kses_post( mypreview_conj_lite_do_shortcode( 'featured_products', array(
-					'per_page' => $per_page,
-					'columns'  => $columns
+			echo wp_kses_post( conj_lite_do_shortcode( 'featured_products', array(
+				'per_page' => $per_page,
+				'columns' => $columns
 			) ) );
 		} elseif ( wc_get_product_ids_on_sale() ) {
-
 			/* translators: 1: Opening heading tag, 2: Closing heading tag. */
 			printf( wp_kses_post( '%1$sOn Sale Now%2$s', 'conj-lite' ), '<h2>', '</h2>' );
 
-			echo wp_kses_post( mypreview_conj_lite_do_shortcode( 'sale_products', array(
-					'per_page' => $per_page,
-					'columns'  => $columns
+			echo wp_kses_post( conj_lite_do_shortcode( 'sale_products', array(
+				'per_page' => $per_page,
+				'columns' => $columns
 			) ) );
 		} elseif ( $recent_fallback ) {
-
 			/* translators: 1: Opening heading tag, 2: Closing heading tag. */
 			printf( wp_kses_post( '%1$sNew In Store%2$s', 'conj-lite' ), '<h2>', '</h2>' );
 
-			echo wp_kses_post( mypreview_conj_lite_do_shortcode( 'recent_products', array(
-					'per_page' => $per_page,
-					'columns'  => $columns,
+			echo wp_kses_post( conj_lite_do_shortcode( 'recent_products', array(
+				'per_page' => $per_page,
+				'columns' => $columns,
 			) ) );
 		} // End If Statement
 
