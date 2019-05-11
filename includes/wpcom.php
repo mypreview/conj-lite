96e1d49ed5c285c57d15a40e4d4f907b14f5cd26
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 15:50:45 +0400

    Add support for wp.com-specific functions

---
+++ includes/wpcom.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions
+ * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+ *
+ * @author  	Mahdi Yazdani
+ * @package 	conj-lite
+ * @since 	    1.1.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; 
+} // End If Statement
+
+/**
+ * Adds support for wp.com-specific `conj-lite` theme functions.
+ *
+ * @uses 	add_theme_support()
+ * @return  void
+ */
+function conj_lite_wpcom_setup() {
+
+	global $themecolors;
+
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		// Whitelist wpcom specific variable intended to be overruled.
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+		$themecolors = array(
+			'bg'      =>  '',
+			'border'  =>  '',
+			'text'    =>  '',
+			'link'    =>  '',
+			'url'     =>  '',
+		);
+	} // End If Statement
+
+	/* Add WP.com print styles */
+	add_theme_support( 'print-styles' );
+
+}
+
+add_action( 'after_setup_theme',   'conj_lite_wpcom_setup',   10 );
