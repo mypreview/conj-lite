ec4c3d77eec5f3a285720d7e7b78b668d2b0d3e4
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 16:18:01 +0400

    Updated hook priorities

--- includes/conj-template-hooks.php
+++ includes/conj-template-hooks.php
@@ -20,21 +20,10 @@ add_action( 'conj_lite_before_site', 		'conj_lite_handheld_navigation',     		  
  * @see  	conj_lite_skip_links()
  * @see  	conj_lite_site_branding()
  * @see  	conj_lite_primary_navigation()
- * @see  	conj_lite_header_image()
  */
 add_action( 'conj_lite_header', 			'conj_lite_skip_links',                         0 );
-add_action( 'conj_lite_header', 			'conj_lite_site_branding',                     30 );
-add_action( 'conj_lite_header', 			'conj_lite_primary_navigation',                60 );
-add_action( 'conj_lite_header', 			'conj_lite_header_image', 					   99 );
-
-/**
- * Site container
- *
- * @see  	conj_lite_site_container_wrapper()
- * @see  	conj_lite_site_container_wrapper_close()
- */
-add_action( 'conj_lite_site_content_top', 	 'conj_lite_site_container_wrapper', 		    5 );
-add_action( 'conj_lite_site_content_bottom', 'conj_lite_site_container_wrapper_close',  	5 );
+add_action( 'conj_lite_header', 			'conj_lite_site_branding',                     10 );
+add_action( 'conj_lite_header', 			'conj_lite_primary_navigation',                40 );
 
 /**
  * Footer
