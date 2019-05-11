fddc3262ad2a8b5704ac2a6625c62eb4c551d0d1
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 14:40:29 +0400

    Updated markup and code tidy

--- footer.php
+++ footer.php
@@ -1,73 +1,46 @@
 <?php
 /**
  * The template for displaying the footer
- *
  * Contains the closing of the #content div and all content after.
  *
  * @link 		https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * @package 	mypreview-conj
+ * @package 	conj-lite
  */
 	
-	/**
-	 * Functions hooked in to mypreview_conj_lite_before_footer action
-	 *
-	 * @hooked mypreview_conj_lite_site_container_wrapper_close		- 5
-	 */
-	do_action( 'mypreview_conj_lite_site_content_bottom' );
-	?>
-
-	</div><!-- #content -->
-
-	<?php
-	/**
-	 * Functions hooked in to mypreview_conj_lite_before_footer action
-	 *
-	 * @hooked mypreview_conj_lite_footer_bar			 - 10
-	 */
-	do_action( 'mypreview_conj_lite_before_footer' ); ?>
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="col-full">
-
-			<?php
-			/**
-			 * Functions hooked in to mypreview_conj_lite_footer action
-			 *
-			 * @hooked mypreview_conj_lite_footer_widgets			 - 10
-			 */
-			do_action( 'mypreview_conj_lite_footer' ); ?>
-
-			<div class="site-info">
+					/**
+					 * Functions hooked in to conj_lite_before_footer action
+					 *
+					 * @hooked conj_lite_site_container_wrapper_close	- 5
+					 */
+					do_action( 'conj_lite_site_content_bottom' );
+					?>
+				</div><!-- #content -->
 
 				<?php
-				/* translators: 1: WordPress.org URL. */
-				printf( esc_html__( '%1$sProudly powered by %2$s ', 'conj-lite' ), '<span class="site-wp-credits">', '<a href="https://wordpress.org" target="_blank">WordPress</a></span>' );
+				do_action( 'conj_lite_before_footer' ); ?>
 
-				printf( wp_kses_post( '%1$s%2$s%3$s' ), '<span>', apply_filters( 'mypreview_conj_lite_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date_i18n( __( 'Y', 'conj-lite' ) ) ), '</span>' );
+				<footer id="colophon" class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="https://schema.org/WPFooter">
+					<div class="col-full">
+						<?php
+						/**
+						 * Functions hooked in to conj_lite_footer action
+						 *
+						 * @hooked conj_lite_footer_widgets	  - 10
+						 * @hooked conj_lite_credit           - 20
+						 */
+						do_action( 'conj_lite_footer' ); ?>
+					</div><!-- .col-full -->
+				</footer><!-- #colophon -->
 
-				/* translators: 1: Seperator, 2: Theme name, 3: Theme author. */
-				printf( esc_html__( '%1$s Theme: %2$s by %3$s', 'conj-lite' ), '<span><span class="sep"> | </span>', esc_html( MYPREVIEW_CONJ_LITE_THEME_NAME ), '<a href="' . esc_url( MYPREVIEW_CONJ_LITE_THEME_URI ) . '">' . esc_html( MYPREVIEW_CONJ_LITE_THEME_AUTHOR ) . '</a>.</span>' );
+				<?php
+				do_action( 'conj_lite_after_footer' ); ?>
+				
+			</div><!-- #page -->
+		</div><!-- #conj-site__wrapper -->
 
-				?>
+		<?php 
+		do_action( 'conj_lite_after_site' );
+		wp_footer(); ?>
 
-			</div><!-- .site-info -->
-		</div><!-- .col-full -->
-	</footer><!-- #colophon -->
-
-	<?php
-	/**
-	 * Functions hooked in to mypreview_conj_lite_before_footer action
-	 *
-	 * @hooked mypreview_conj_lite_wc_add_to_cart_bar		- 999
-	 */
-	do_action( 'mypreview_conj_lite_after_footer' ); ?>
-	
-</div><!-- #page -->
-
-<?php 
-do_action( 'mypreview_conj_lite_after_site' );
-
-wp_footer(); ?>
-
-</body>
+	</body>
 </html>
