931d8677c27184d840c7c4c65c4c88fd21ce94b9
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 14:41:40 +0400

    Updated prefix and code tidy

--- sidebar.php
+++ sidebar.php
@@ -3,16 +3,16 @@
  * The sidebar containing the main widget area
  *
  * @link 		https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * @package 	mypreview-conj
+ * @package 	conj-lite
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) || apply_filters( 'mypreview_conj_lite_disable_sidebar', FALSE ) ) {
+if ( ! is_active_sidebar( 'sidebar-1' ) || apply_filters( 'conj_lite_disable_sidebar', FALSE ) ) {
 	return;
 }
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary" itemscope="itemscope" itemtype="https://schema.org/WPSideBar">
-	<div class="conj-secondary-widget-area__wrapper">
+	<div class="conj-lite-secondary-widget-area__wrapper">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 </aside><!-- #secondary -->
