30cb73abd7f2ca61bbf6fa703a18d31a38bf5122
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 15:53:38 +0400

    Updated hook method names

--- includes/conj-template-hooks.php
+++ includes/conj-template-hooks.php
@@ -2,41 +2,45 @@
 /**
  * Conj Lite hooks
  *
- * @package 	mypreview-conj
+ * @author  	Mahdi Yazdani
+ * @package 	conj-lite
+ * @since 	    1.1.0
  */
 
 /**
  * Before site
  *
- * @see  mypreview_conj_lite_handheld_navigation()
+ * @see  	conj_lite_handheld_navigation()
  */
-add_action( 'mypreview_conj_lite_before_site', 	'mypreview_conj_lite_handheld_navigation',     				10 );
+add_action( 'conj_lite_before_site', 		'conj_lite_handheld_navigation',     		  10 );
 
 /**
  * Header
  *
- * @see  mypreview_conj_lite_skip_links()
- * @see  mypreview_conj_lite_site_branding()
- * @see  mypreview_conj_lite_primary_navigation()
- * @see  mypreview_conj_lite_header_image()
+ * @see  	conj_lite_skip_links()
+ * @see  	conj_lite_site_branding()
+ * @see  	conj_lite_primary_navigation()
+ * @see  	conj_lite_header_image()
  */
-add_action( 'mypreview_conj_lite_header', 			'mypreview_conj_lite_skip_links',                         0 );
-add_action( 'mypreview_conj_lite_header', 			'mypreview_conj_lite_site_branding',                     30 );
-add_action( 'mypreview_conj_lite_header', 			'mypreview_conj_lite_primary_navigation',                60 );
-add_action( 'mypreview_conj_lite_header', 			'mypreview_conj_lite_header_image', 					 99 );
+add_action( 'conj_lite_header', 			'conj_lite_skip_links',                         0 );
+add_action( 'conj_lite_header', 			'conj_lite_site_branding',                     30 );
+add_action( 'conj_lite_header', 			'conj_lite_primary_navigation',                60 );
+add_action( 'conj_lite_header', 			'conj_lite_header_image', 					   99 );
 
 /**
  * Site container
  *
- * @see  mypreview_conj_lite_site_container_wrapper()
- * @see  mypreview_conj_lite_site_container_wrapper_close()
+ * @see  	conj_lite_site_container_wrapper()
+ * @see  	conj_lite_site_container_wrapper_close()
  */
-add_action( 'mypreview_conj_lite_site_content_top', 	 'mypreview_conj_lite_site_container_wrapper', 		  5 );
-add_action( 'mypreview_conj_lite_site_content_bottom',	 'mypreview_conj_lite_site_container_wrapper_close',  5 );
+add_action( 'conj_lite_site_content_top', 	 'conj_lite_site_container_wrapper', 		    5 );
+add_action( 'conj_lite_site_content_bottom', 'conj_lite_site_container_wrapper_close',  	5 );
 
 /**
  * Footer
  *
- * @see  mypreview_conj_lite_footer_widgets()
+ * @see  	conj_lite_footer_widgets()
+ * @see  	conj_lite_credit()
  */
-add_action( 'mypreview_conj_lite_footer', 			 'mypreview_conj_lite_footer_widgets',			  		  10 );
+add_action( 'conj_lite_footer', 			 'conj_lite_footer_widgets',			  	   10 );
+add_action( 'conj_footer', 			 		 'conj_lite_credit',					  	   20 );
