da09128af19e2dca23c764924bab17aea902d0a0
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-11 14:41:14 +0400

    Code tidy

--- page.php
+++ page.php
@@ -8,19 +8,17 @@
  * different template.
  *
  * @link 		https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package 	mypreview-conj
+ * @package 	conj-lite
  */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
 			<?php
 			while ( have_posts() ) : the_post();
 
-				do_action( 'mypreview_conj_lite_page_before' );
+				do_action( 'conj_lite_page_before' );
 
 				get_template_part( 'template-parts/content', 'page' );
 
@@ -29,11 +27,10 @@ get_header(); ?>
 					comments_template();
 				} // End If Statement
 
-				do_action( 'mypreview_conj_lite_page_after' );
+				do_action( 'conj_lite_page_after' );
 
 			endwhile; // End of the loop.
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
