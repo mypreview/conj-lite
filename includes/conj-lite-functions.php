183672434c40aabcddb116f66cbb1f22e9fe85ce
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-12 16:47:08 +0400

    Added new method to adjust color brightness

--- includes/conj-lite-functions.php
+++ includes/conj-lite-functions.php
@@ -375,6 +375,53 @@ if ( ! function_exists( 'conj_lite_site_title_or_logo' ) ) :
 endif;
 
 /**
+ * Adjust a hex color brightness
+ * Allows us to create hover styles for custom link colors
+ *
+ * @uses 	max()
+ * @uses 	min()
+ * @uses 	str_replace()
+ * @uses 	str_repeat()
+ * @uses 	substr()
+ * @uses 	hexdec()
+ * @uses 	str_pad()
+ * @param  	string  	$hex   		Gex color e.g. #111111.
+ * @param  	integer 	$steps 		Factor by which to brighten/darken ranging from -255 (darken) to 255 (brighten).
+ * @return 	string        			Brightened/darkened hex color
+ */
+if ( ! function_exists( 'conj_lite_adjust_color_brightness' ) ) :
+	function conj_lite_adjust_color_brightness( $hex, $steps ) {
+
+		// Steps should be between -255 and 255. Negative = darker, positive = lighter.
+		$steps = max( -255, min( 255, $steps ) );
+
+		// Format the hex color string.
+		$hex = str_replace( '#', '', $hex );
+
+		if ( 3 == strlen( $hex ) ) {
+			$hex = str_repeat( substr( $hex, 0, 1 ), 2 ) . str_repeat( substr( $hex, 1, 1 ), 2 ) . str_repeat( substr( $hex, 2, 1 ), 2 );
+		} // End If Statement
+
+		// Get decimal values.
+		$r = hexdec( substr( $hex, 0, 2 ) );
+		$g = hexdec( substr( $hex, 2, 2 ) );
+		$b = hexdec( substr( $hex, 4, 2 ) );
+
+		// Adjust number of steps and keep it inside 0 to 255.
+		$r = max( 0, min( 255, $r + $steps ) );
+		$g = max( 0, min( 255, $g + $steps ) );
+		$b = max( 0, min( 255, $b + $steps ) );
+
+		$r_hex = str_pad( dechex( $r ), 2, '0', STR_PAD_LEFT );
+		$g_hex = str_pad( dechex( $g ), 2, '0', STR_PAD_LEFT );
+		$b_hex = str_pad( dechex( $b ), 2, '0', STR_PAD_LEFT );
+
+		return '#' . $r_hex . $g_hex . $b_hex;
+
+	}
+endif;
+
+/**
  * Shim for sites older than 5.2.
  *
  * @link 	https://core.trac.wordpress.org/ticket/12563
