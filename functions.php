610ce4ea825235a18325e011449578add9c82847
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-12 15:22:26 +0400

    Included customizer styles file path

--- functions.php
+++ functions.php
@@ -37,7 +37,8 @@ require get_parent_theme_file_path( '/includes/back-compat.php' );
  */
 $conj_lite = ( object )array(
 	'version' => CONJ_LITE_THEME_NAME,
-	'main' => require get_parent_theme_file_path( '/includes/class-conj-lite.php' )
+	'main' => require get_parent_theme_file_path( '/includes/class-conj-lite.php' ),
+	'customizer_styles'	=> require get_parent_theme_file_path( '/includes/customizer/class-conj-lite-customizer-styles.php' )
 );
 
 require get_parent_theme_file_path( '/includes/conj-lite-functions.php' );
