abcbb792c3947be46f14964c80fd0926943f064c
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-05-13 15:26:24 +0400

    New method to dismiss up-sell banner

---
+++ assets/admin/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * WordPress admin enhancements, specific to theme's up-sell admin notice.
+ *
+ * @requires 	wp-block-editor
+ * @package     conj-lite
+ * @author     	MyPreview (Github: @mahdiyazdani, @mypreview)
+ */
+
+( function( wp, $ ) {
+	'use strict';
+
+	if ( ! wp ) {
+		return;
+	} // End If Statement
+
+	$( function() {
+		// Dismiss up-sell banner notice on user click!
+		$( document ).on( 'click', '#conj-lite-upsell-notice .notice-dismiss', function() {
+			$.ajax({
+				type: 'POST',
+				url: ajaxurl,
+				data: { 
+					nonce: conj_lite_vars.dismiss_upsell_notice_nonce, 
+					action: 'conj_lite_dismiss_upsell_notice' 
+				},
+				dataType: 'json'
+			} );
+		} );
+	} );
+
+} )( window.wp, jQuery );
