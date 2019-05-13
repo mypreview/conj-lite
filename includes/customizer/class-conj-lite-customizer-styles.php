20af21d1af4737e4af7148bfe28945621545a51d
Sajedeh Gooklani <gookalani@mail.com>
2019-05-13 23:53:44 +0400

    Update class-conj-lite-customizer-styles.php
    Add border color to header.
    Add heading color to search result heading, comment author, comment reply title, calendar caption, widget title, heading tags and describe in a description list.
    Add paragraph color to site navigation link, quote tag, calendar dates, post tag links, meta links in attachment posts,  post meta links, author of post, link in list widgets,  categories link in categories block, paragraph.
    Add link color to links, page numbers, comment links, hovered link in list widgets, archives block link, latest posts block, checkbox checked icon, mark tag and chat post format border.
    Add button color to input type submit, button tag, input type button, links with button class, today date of calendar, tag cloud widget, file block button, date picker select tag, button block,

--- includes/customizer/class-conj-lite-customizer-styles.php
+++ includes/customizer/class-conj-lite-customizer-styles.php
@@ -35,6 +35,20 @@ if ( ! class_exists( 'Conj_Lite_Customizer_Styles' ) ) :
         	// General
 			$general_site_title_color = get_header_textcolor();
 			$general_background_color = sanitize_hex_color_no_hash( get_background_color() );
+			$general_background_color_dark = sanitize_hex_color( conj_lite_adjust_color_brightness( $general_background_color, -20 ) );
+			$general_background_color_darker = sanitize_hex_color( conj_lite_adjust_color_brightness( $general_background_color, -5 ) );
+			$general_heading_color = sanitize_hex_color( get_theme_mod( 'conj_lite_general_heading_color', '#464855' ) );
+			$general_text_color = sanitize_hex_color( get_theme_mod( 'conj_lite_general_text_color', '#6B6F81' ) );
+			$general_text_color_light = sanitize_hex_color( conj_lite_adjust_color_brightness( $general_text_color, 30 ) );
+			$general_text_color_lighter = sanitize_hex_color( conj_lite_adjust_color_brightness( $general_text_color, 50 ) );
+			$general_link_color = sanitize_hex_color( get_theme_mod( 'conj_lite_general_link_color', '#666EE8' ) );
+			$general_link_color_lighter = sanitize_hex_color( conj_lite_adjust_color_brightness( $general_link_color, 80 ) );
+			$conj_lite_general_link_alt_color = sanitize_hex_color( get_theme_mod( 'conj_general_link_alt_color', '#414B92' ) )
+			// Button
+			$button_text_color = sanitize_hex_color( get_theme_mod( 'conj_lite_button_text_color', '#FFFFFF' ) );
+			$button_alt_text_color = sanitize_hex_color( get_theme_mod( 'conj_lite_button_alt_text_color', '#666EE8' ) );
+			$button_background_color = sanitize_hex_color( get_theme_mod( 'conj_lite_button_background_color', '#666EE8' ) );
+			$button_background_color_lighter = sanitize_hex_color( conj_adjust_color_brightness( $button_background_color, 50 ) );
 			// Header
 			$header_background_attachment_url = ( get_header_image() ) 	?  esc_url( get_header_image() ) : NULL;
 
@@ -49,6 +63,270 @@ if ( ! class_exists( 'Conj_Lite_Customizer_Styles' ) ) :
 				.conj-lite-blog__archive:not(.has-sidebar) article.post.hentry .alignfull,
 				body {
 					background-color: #{$general_background_color};
+				}
+				#masthead {
+					border-color: {$general_background_color_darker};
+				}
+				body.search-results article .entry-footer a,
+				body.search-results article .entry-title a,
+				.search-results:not(.post-type-archive) .site-main article .entry-header a,
+				body:not(.single-post) article.post.hentry .entry-header a,
+				.comment-meta .comment-author .fn,
+				.comment-respond .comment-reply-title,
+				.comment-respond #reply-title,
+				.main-navigation .widget_calendar .calendar_wrap caption,
+				.wp-block-calendar table caption,
+				.conj-lite-secondary-widget-area__wrapper .widget_calendar .calendar_wrap caption,
+				.conj-lite-secondary-widget-area__wrapper .widget-title,
+				dd,
+				h1,
+				h2,
+				h3,
+				h4,
+				h5,
+				h6 {
+					color: {$general_heading_color};
+				}
+				#site-navigation > div.menu > ul > li > a,
+				.primary-navigation > ul.menu > .menu-item > a,
+				.wp-block-pullquote,
+				.wp-block-quote__citation,
+				.wp-block-quote footer,
+				blockquote,
+				.wp-block-calendar table tbody,
+				.ui-datepicker .ui-datepicker-calendar th,
+				body.single-post article.post.hentry .entry-footer .tags-links > a,
+				.type-attachment .post-meta a,
+				article.post.hentry .post-meta a,
+				.type-attachment .post-meta .byline a,
+				.single-post article.type-post .post-meta .byline a,
+				.error-404-first .widget_recent_entries a,
+				.wp-block-latest-posts a,
+				.conj-lite-secondary-widget-area__wrapper .widget_recent_entries a,
+				.conj-lite-secondary-widget-area__wrapper .widget_pages ul a,
+				.conj-lite-secondary-widget-area__wrapper .widget ul.menu li > a,
+				.conj-lite-secondary-widget-area__wrapper .widget_meta a,
+				.widget_categories a,
+				.wp-block-categories.wp-block-categories-list a,
+				.conj-lite-secondary-widget-area__wrapper .widget_archive a,
+				ul.wp-block-archives a,
+				body {
+					color: {$general_text_color};
+				}
+				.wp-block-tag-cloud a.tag-cloud-link,
+				.page-links a>.page-number,
+				.nav-links a,
+				.pagination .page-numbers:not(.current),
+				.search-results:not(.post-type-archive) .site-main article:not(.type-product):not(.type-post) .entry-meta span,
+				article.post.hentry .cat-tags-links .post-categories a,
+				.widget_tag_cloud a,
+				label,
+				dt {
+					color: {$general_text_color_light};
+				}
+				.comment.byuser img.avatar,
+				.comment-metadata a time,
+				.comment-meta .comment-author .says,
+				.wp-block-calendar tfoot a,
+				.calendar_wrap tfoot td a,
+				.wp-block-calendar thead th,
+				.calendar_wrap thead tr th,
+				.type-attachment .post-meta .byline,
+				.single-post article.type-post .post-meta .byline {
+					color: {$general_text_color_lighter};
+				}
+				body.search-results article .entry-footer a:hover,
+				body.search-results article .entry-title a:hover,
+				body.single-post article.post.hentry .entry-footer .tags-links > a:hover,
+				.single-post article.type-post .post-meta .byline a:hover,
+				.type-attachment .post-meta .byline a:hover,
+				.search-results:not(.post-type-archive) .site-main article .entry-header a:hover,
+				body:not(.single-post) article.post.hentry .entry-header a:hover,
+				.type-attachment .post-meta a:hover,
+				article.post.hentry .post-meta a:hover,
+				.entry-footer .edit-link .post-edit-link,
+				.page-links a>.page-number:hover,
+				.site-main .comment-navigation a:hover,
+				.post-navigation .nav-links a:hover,
+				.pagination .page-numbers:not(.dots):hover,
+				.pagination .page-numbers.current,
+				body:not(.single-post) article.type-post .page-links > a:first-child .page-number,
+				.page-links .page-number,
+				.comment-metadata a:hover time,
+				.error-404-first .widget_recent_entries a:hover,
+				.conj-lite-secondary-widget-area__wrapper .widget_recent_entries a:hover,
+				.conj-lite-secondary-widget-area__wrapper .widget_categories .current-cat-parent > a,
+				.conj-lite-secondary-widget-area__wrapper .widget_categories .current-cat > a,
+				.conj-lite-secondary-widget-area__wrapper ul.menu .current_page_parent > a,
+				.conj-lite-secondary-widget-area__wrapper ul.menu .current_page_item > a,
+				.conj-lite-secondary-widget-area__wrapper .widget_pages .current_page_parent > a,
+				.conj-lite-secondary-widget-area__wrapper .widget_pages .current_page_item > a,
+				.conj-lite-secondary-widget-area__wrapper .widget_pages ul a:hover,
+				.conj-lite-secondary-widget-area__wrapper .widget ul.menu li:hover > a,
+				.conj-lite-secondary-widget-area__wrapper .widget_meta a abbr,
+				.conj-lite-secondary-widget-area__wrapper .widget_meta a:hover,
+				.wp-block-categories.wp-block-categories-list a:hover,
+				.widget_categories a:not(.has-text-color):hover,
+				.wp-block-calendar tfoot a:hover,
+				.calendar_wrap tfoot td a:hover,
+				.conj-lite-secondary-widget-area__wrapper .widget_archive a:hover,
+				ul.wp-block-archives a:hover,
+				.wp-block-latest-posts > li > a:hover,
+				.site-branding .site-title a:hover,
+				input[type='checkbox']:checked:before,
+				a {
+					color: {$general_link_color};
+				}
+				mark,
+				input[type='radio']:checked:before {
+					background-color: {$general_link_color};
+				}
+				body:not(.single-post) article.type-post .page-links > a:first-child .page-number,
+				article.post.hentry.format-chat .entry-content > *:nth-child(even),
+				article.post.hentry.format-chat .entry-content > *:nth-child(even):before {
+					border-color: {$general_link_color};
+				}
+				a:hover {
+					color: {$general_link_alt_color};
+				}
+				/* Button */
+				.wp-block-tag-cloud a.tag-cloud-link:hover,
+				.ui-datepicker .ui-datepicker-calendar td a:hover,
+				.ui-datepicker .ui-datepicker-calendar td a.ui-state-active,
+				.ui-datepicker .ui-datepicker-title select,
+				.ui-datepicker .ui-datepicker-header,
+				.wp-block-button:not(.is-style-outline) .wp-block-button__link:not(.has-background),
+				body.single-post article.post.hentry .entry-footer .tags-links > a:hover,
+				article.post.hentry .cat-tags-links .post-categories a:hover,
+				body:not(.single-post) article.post.hentry .entry-content .more-link,
+				.wp-block-file .wp-block-file__button,
+				.widget_tag_cloud ul li:hover,
+				.wp-block-calendar tbody td#today,
+				.calendar_wrap #today,
+				.button:not(:focus):not(:active):not(:hover):not([disabled]):not([name='apply_coupon']), 
+				button:not(.close-btn):not(.handheld-menu-toggle):not(.pswp__button):not(:focus):not(:active):not(:hover):not([disabled]):not([name='apply_coupon']), 
+				input[type='button']:not(:focus):not(:active):not(:hover), 
+				input[type='reset']:not(:focus):not(:active):not(:hover), 
+				button[name='apply_coupon']:active,
+				button[name='apply_coupon']:hover,
+				button[name='apply_coupon']:focus,
+				input[type='submit'][name='apply_coupon']:hover,
+				input[type='submit'][name='apply_coupon']:focus,
+				input[type='submit']:not([name='apply_coupon']):not(:focus):not(:active):not(:hover) {
+					background-color: {$button_background_color};
+				}
+				.wp-block-button.is-style-outline .wp-block-button__link:not(.has-text-color) {
+					color: {$button_background_color};
+				}
+				.wp-block-file a.wp-block-file__button,
+				.wp-block-button.is-style-outline .wp-block-button__link:not(.has-text-color),
+				body:not(.single-post) article.post.hentry .entry-content .more-link,
+				.button:not(:focus):not(:active):not(:hover):not([disabled]):not([name='apply_coupon']), 
+				button:not(.close-btn):not(.handheld-menu-toggle):not(.pswp__button):not(:focus):not(:active):not(:hover):not([disabled]):not([name='apply_coupon']), 
+				input[type='button']:not(:focus):not(:active):not(:hover), 
+				input[type='reset']:not(:focus):not(:active):not(:hover), 
+				button[name='apply_coupon']:active,
+				button[name='apply_coupon']:hover,
+				button[name='apply_coupon']:focus,
+				input[type='submit'][name='apply_coupon']:hover,
+				input[type='submit'][name='apply_coupon']:focus,
+				input[type='submit']:not([name='apply_coupon']):not(:focus):not(:active):not(:hover) {
+					border-color: {$button_background_color};
+				}
+				.wp-block-calendar tbody td > a,
+				.widget_calendar .calendar_wrap tbody td > a {
+					background-color: {$button_background_color_lighter};
+				}
+				.wp-block-tag-cloud a.tag-cloud-link:hover,
+				#subscribe-submit [type='submit']:hover,
+				.ui-datepicker.ui-widget .ui-datepicker-header,
+				.ui-datepicker .ui-datepicker-calendar td a:hover,
+				.ui-datepicker .ui-datepicker-calendar td a.ui-state-active,
+				.ui-datepicker .ui-datepicker-title select,
+				.ui-datepicker-header .ui-datepicker-next, 
+				.ui-datepicker-header .ui-datepicker-prev,
+				.ui-datepicker-header .ui-datepicker-next:hover, 
+				.ui-datepicker-header .ui-datepicker-prev:hover,
+				.wp-block-button:not(.is-style-outline) .wp-block-button__link:not(.has-text-color):hover,
+				.wp-block-button:not(.is-style-outline) .wp-block-button__link:not(.has-text-color),
+				body.single-post article.post.hentry .entry-footer .tags-links > a:hover,
+				article.post.hentry .cat-tags-links .post-categories a:hover,
+				body:not(.single-post) article.post.hentry .entry-content .more-link,
+				.wp-block-file .wp-block-file__button,
+				.widget_tag_cloud ul li:hover a,
+				.wp-block-calendar tbody td > a,
+				.widget_calendar .calendar_wrap tbody td > a,
+				.wp-block-calendar tbody td#today,
+				.calendar_wrap #today,
+				.button:not(:focus):not(:active):not(:hover):not([disabled]):not([name='apply_coupon']), 
+				button:not(.close-btn):not(.handheld-menu-toggle):not(.pswp__button):not(:focus):not(:active):not(:hover):not([disabled]):not([name='apply_coupon']), 
+				input[type='button']:not(:focus):not(:active):not(:hover), 
+				input[type='reset']:not(:focus):not(:active):not(:hover), 
+				button[name='apply_coupon']:active,
+				button[name='apply_coupon']:hover,
+				button[name='apply_coupon']:focus,
+				input[type='submit'][name='apply_coupon']:hover,
+				input[type='submit'][name='apply_coupon']:focus,
+				input[type='submit']:not([name='apply_coupon']):not(:focus):not(:active):not(:hover) {
+					color: {$button_text_color};
+				}
+				.ui-datepicker .ui-datepicker-title select {
+					background-image: -webkit-linear-gradient(45deg, transparent 50%, {$button_text_color} 50%), -webkit-linear-gradient(315deg, {$button_text_color} 50%, transparent 50%), -webkit-linear-gradient(left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0));
+					background-image: -o-linear-gradient(45deg, transparent 50%, {$button_text_color} 50%), -o-linear-gradient(315deg, {$button_text_color} 50%, transparent 50%), -o-linear-gradient(left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0));
+					background-image: linear-gradient(45deg, transparent 50%, {$button_text_color} 50%), linear-gradient(135deg, {$button_text_color} 50%, transparent 50%), linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0));
+				}
+				.wp-block-button.is-style-outline .wp-block-button__link:not(.has-text-color):hover,
+				.wp-block-file a.wp-block-file__button:active, 
+				.wp-block-file a.wp-block-file__button:focus, 
+				.wp-block-file a.wp-block-file__button:hover, 
+				.wp-block-file a.wp-block-file__button:visited,
+				body:not(.single-post) article.post.hentry .entry-content .more-link:hover,
+				.button:not([name='apply_coupon']):focus, 
+				.button[disabled], 
+				button[disabled], 
+				button:not(.close-btn):not(.handheld-menu-toggle):not(.pswp__button):not([name='apply_coupon']):focus, 
+				input[type='button']:focus, 
+				input[type='reset']:focus, 
+				button[name='apply_coupon'],
+				input[type='submit'][name='apply_coupon'],
+				input[type='submit']:not([name='apply_coupon']):focus,
+				.button:not([name='apply_coupon']):active, 
+				button:not(.close-btn):not(.handheld-menu-toggle):not(.pswp__button):not([name='apply_coupon']):active, 
+				input[type='button']:active, 
+				input[type='reset']:active, 
+				input[type='submit']:not([name='apply_coupon']):active,
+				.button:not([name='apply_coupon']):hover, 
+				button:not(.close-btn):not(.handheld-menu-toggle):not(.pswp__button):not([name='apply_coupon']):hover, 
+				input[type='button']:hover, 
+				input[type='reset']:hover, 
+				input[type='submit']:not([name='apply_coupon']):hover {
+					color: {$button_alt_text_color};
+				}
+				.wp-block-file a.wp-block-file__button:active, 
+				.wp-block-file a.wp-block-file__button:focus, 
+				.wp-block-file a.wp-block-file__button:hover, 
+				.wp-block-file a.wp-block-file__button:visited,
+				.button:active, 
+				button:active, 
+				button[disabled],
+				input[type='button']:active,
+				input[type='submit']:active,
+				.button:focus, 
+				button:focus, 
+				input[type='button']:focus,
+				input[type='submit']:focus,
+				.button:hover, 
+				button:hover,
+				button[name='apply_coupon'],
+				input[type='submit'][name='apply_coupon'],
+				input[type='button']:hover,
+				input[type='submit']:hover, 
+				.wp-block-button.is-style-outline .wp-block-button__link:not(.has-text-color):hover,
+				body:not(.single-post) article.post.hentry .entry-content .more-link:hover {
+					border-color: {$button_alt_text_color};
+				}
+				.wp-block-button:not(.is-style-outline) .wp-block-button__link:not(.has-background):hover {
+					background-color: {$button_alt_text_color};
 				}
 				/* Header */
 				body.has-header-image #masthead {
