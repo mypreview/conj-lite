94be74eaf77469eda60fdb2d262613f5fa09991f
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-17 10:39:11 +0400

    Added site wrapper col-full methods
    Extracted static HTML markup from PHP
    Updated footer widget display statement with a built-in method
    Updated site credits markup
    Updated with BEM compatible classnames

--- includes/conj-lite-template-functions.php
+++ includes/conj-lite-template-functions.php
@@ -2,7 +2,7 @@
 /**
  * Conj Lite template functions.
  *
- * @since 	    1.1.0
+ * @since 	    1.2.0
  * @package 	conj-lite
  * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
  */
@@ -16,24 +16,14 @@
 if ( ! function_exists( 'conj_lite_handheld_navigation' ) ) :
 	function conj_lite_handheld_navigation() {
 
-		?>
-		<div id="left" class="handheld-offcanvas c-offcanvas is-hidden" role="complementary">
-			<button class="close-btn">
-				<i class="feather-x"></i>
-			</button>
-			<div id="handheld-slinky-menu">
-				<?php
-				    wp_nav_menu(
-					    array(
-						    'theme_location' => 'handheld',
-							'container_class' => 'handheld-navigation'
-					    )
-				    );
-			    ?>	
-			</div><!-- #handheld-slinky-menu -->
-			<div class="offcanvas-nav d-block d-md-none" data-set="bs"></div>
-		</div><!-- .handheld-offcanvas -->
-		<?php	
+		?><div id="handheld-offcanvas" class="handheld-offcanvas c-offcanvas is-hidden" role="complementary"><button class="close-btn"><i class="feather-x"></i></button><div id="handheld-slinky-menu"><?php
+			wp_nav_menu(
+			    apply_filters( 'conj_lite_handheld_nav_menu_args', array(
+				    'theme_location' => 'handheld',
+					'container_class' => 'handheld-navigation'
+			    ) )
+		    );
+		?></div></div><div class="offcanvas-nav d-block d-md-none" data-set="bs"></div></div><?php
 
 	}
 endif;
@@ -46,10 +36,10 @@ endif;
 if ( ! function_exists( 'conj_lite_skip_links' ) ) :
 	function conj_lite_skip_links() {
 
-		?>
-		<a class="skip-link screen-reader-text" href="#site-navigation"><?php esc_attr_e( 'Skip to navigation', 'conj-lite' ); ?></a>
-		<a class="skip-link screen-reader-text" href="#content"><?php esc_attr_e( 'Skip to content', 'conj-lite' ); ?></a>
-		<?php	
+		/* translators: 1: Open anchor tag, 2: Close anchor tag. */
+		printf( esc_html_x( '%1$sSkip to navigation%2$s', 'screen reader text', 'conj-lite' ), '<a class="skip-link screen-reader-text" href="#site-navigation">', '</a>' );
+		/* translators: 1: Open anchor tag, 2: Close anchor tag. */
+		printf( esc_html_x( '%1$sSkip to content%2$s', 'screen reader text', 'conj-lite' ), '<a class="skip-link screen-reader-text" href="#content">', '</a>' );	
 
 	}
 endif;
@@ -63,11 +53,9 @@ endif;
 if ( ! function_exists( 'conj_lite_site_branding' ) ) :
 	function conj_lite_site_branding() {
 
-		?>
-		<div class="site-branding">
+		?><div class="site-branding">
 			<?php conj_lite_site_title_or_logo(); ?>
-		</div><!-- .site-branding -->
-		<?php
+		</div><!-- .site-branding --><?php
 
 	}
 endif;
@@ -81,28 +69,43 @@ endif;
 if ( ! function_exists( 'conj_lite_primary_navigation' ) ) :
 	function conj_lite_primary_navigation() {
 
-		?>
-		<div class="primary-navigation">
-			<nav id="site-navigation" class="main-navigation" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement">
-				<button class="handheld-menu-toggle js-offcanvas-toggler js-handheld-offcanvas-toggler">
-					<?php esc_html_e( 'Menu', 'conj-lite' ); ?>
-					<span class="hamburger-nav-icon">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</span>
-				</button><!-- .handheld-menu-toggle -->
-				<?php
-					wp_nav_menu(
-						array(
-							'theme_location'  => 'primary',
-							'container_class' => 'primary-navigation'
-						)
-					);
-				?>
-			</nav><!-- #site-navigation -->
-		</div><!-- .primary-navigation -->
-		<?php
+		?><div class="primary-navigation">
+			<nav id="site-navigation" class="main-navigation" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement"><?php
+				/* translators: 1: Open `push-menu` button tag, 2: Close `push-menu` close tag(s). */
+				printf( esc_html__( '%1$sMenu%2$s', 'conj' ), '<button class="handheld-menu-toggle js-offcanvas-toggler js-handheld-offcanvas-toggler">', '<span class="hamburger-nav-icon"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></span></button>' );
+				wp_nav_menu(
+					apply_filters( 'conj_lite_primary_nav_menu_args', array(
+						'theme_location' => 'primary',
+						'container_class' => 'primary-navigation'
+					) )
+				);
+		?></nav></div><?php
+
+	}
+endif;
+
+/**
+ * Site page container wrapper.
+ *
+ * @return  void
+ */
+if ( ! function_exists( 'conj_lite_site_container_wrapper' ) ) :
+	function conj_lite_site_container_wrapper() {
+
+		?><div class="col-full"><?php
+
+	}
+endif;
+
+/**
+ * Site page container wrapper close.
+ *
+ * @return  void
+ */
+if ( ! function_exists( 'conj_lite_site_container_wrapper_close' ) ) :
+	function conj_lite_site_container_wrapper_close() {
+
+		?></div><?php
 
 	}
 endif;
@@ -110,14 +113,14 @@ endif;
 /**
  * Display the footer widget regions.
  *
- * @uses	dynamic_sidebar()
+ * @uses	conj_lite_get_dynamic_sidebar()
  * @uses	is_active_sidebar()
  * @return  void
  */
 if ( ! function_exists( 'conj_lite_footer_widgets' ) ) :
 	function conj_lite_footer_widgets() {
 
-		$rows = intval( apply_filters( 'conj_lite_footer_widget_rows', 2 ) );
+		$rows = intval( apply_filters( 'conj_lite_footer_widget_rows', 1 ) );
 		$regions = intval( apply_filters( 'conj_lite_footer_widget_columns', 4 ) );
 
 		for ( $row = 1; $row <= $rows; $row++ ) :
@@ -127,25 +130,22 @@ if ( ! function_exists( 'conj_lite_footer_widgets' ) ) :
 					$columns = $region;
 					break;
 				} // End If Statement
-			}
+			} // End For Loop
 
-			if ( isset( $columns ) ) : ?>
-				<div class=<?php echo '"footer-widgets row-' . strval( $row ) . ' col-' . strval( $columns ) . ' fix"'; ?>><?php
+			if ( isset( $columns ) ) :
+				printf( '<div class="footer-widgets row-%d col-%d fix">', strval( $row ), strval( $columns ) );
 					for ( $column = 1; $column <= $columns; $column++ ) :
 						$footer_n = $column + $regions * ( $row - 1 );
 
-						if ( is_active_sidebar( 'footer-' . strval( $footer_n ) ) ) : ?>
-
-							<div class="block footer-widget-<?php echo strval( $column ); ?>">
-								<?php dynamic_sidebar( 'footer-' . strval( $footer_n ) ); ?>
-							</div><?php
-
+						// Determines whether a sidebar is in use.
+						if ( is_active_sidebar( 'footer-' . strval( $footer_n ) ) ) :
+							printf( '<div class="block footer-widget-%1$s">%2$s</div>', strval( $column ), conj_lite_get_dynamic_sidebar( 'footer-' . strval( $footer_n ) ) ); // WPCS: XSS okay.
 						endif;
-					endfor; ?>
-				</div><!-- .footer-widgets.row-<?php echo strval( $row ); ?> --><?php
+					endfor;
+				?></div><!-- .footer-widgets --><?php
 				unset( $columns );
-			endif;
-		endfor;		
+			endif; // End If Statement
+		endfor; // End For Loop		
 
 	}
 endif;
@@ -160,21 +160,18 @@ endif;
 if ( ! function_exists( 'conj_lite_credit' ) ) :
 	function conj_lite_credit() {
 
+		$copyright_text = sprintf( '&copy; %s&nbsp;%s', get_bloginfo( 'name' ), date_i18n( esc_html_x( 'Y', 'date in localized format', 'conj-lite' ) ) );
+
 		?><div class="site-info"><?php 
+			if ( apply_filters( 'conj_lite_credit_wp_link', TRUE ) ) {
+				/* translators: 1: WordPress.org URL. */
+				printf( esc_html__( '%1$sProudly powered by %2$s ', 'conj-lite' ), '<span class="site-info__wp-credits">', '<a href="https://wordpress.org" target="_blank" rel="noopener noreferrer nofollow">WordPress</a></span>' );
+			} // End If Statement
 
-		if ( apply_filters( 'conj_lite_credit_wp_link', TRUE ) ) {
-			/* translators: 1: WordPress.org URL. */
-			printf( esc_html__( '%1$sProudly powered by %2$s ', 'conj-lite' ), '<span class="site-wp-credits">', '<a href="https://wordpress.org" target="_blank" rel="noopener noreferrer nofollow">WordPress</a></span>' );
-		} // End If Statement
-
-		printf( wp_kses_post( '%1$s%2$s%3$s' ), '<span class="site-copyright">', apply_filters( 'conj_lite_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date_i18n( __( 'Y', 'conj-lite' ) ) ), '</span>' );
-		 
-		if ( apply_filters( 'conj_lite_credit_author_link', TRUE ) ) {
+			printf( wp_kses_post( '%1$s%2$s%3$s' ), '<span class="site-info__copyright">', apply_filters( 'conj_lite_copyright_text', wp_kses_post( $copyright_text ) ), '</span>' );
 			/* translators: 1: Seperator, 2: Theme name, 3: Theme author. */
-			printf( esc_html__( '%1$s Theme: %2$s by %3$s', 'conj-lite' ), '<span class="site-author-credits"><span class="sep"> | </span>', esc_html( CONJ_LITE_THEME_NAME ), '<a href="' . esc_url( CONJ_LITE_THEME_AUTHOR_URI ) . '" target="_blank" rel="noopener noreferrer nofollow">' . esc_html( CONJ_LITE_THEME_AUTHOR ) . '</a>.</span>' );
-		} // End If Statement ?>
-
-		</div><!-- .site-info --><?php		
+			printf( esc_html__( '%1$s Theme: %2$s by %3$s', 'conj-lite' ), '<span class="site-info__author-credits"><span class="sep"> | </span>', esc_html( CONJ_THEME_NAME ), sprintf( '<a href="%s" target="_blank" rel="noopener noreferrer">%s</a></span>', esc_url( CONJ_THEME_URI ), esc_html( CONJ_THEME_AUTHOR ) ) ); ?>
+		</div><!-- .site-info --><?php	
 
 	}
 endif;
