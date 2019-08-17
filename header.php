9269cd71a2666f505904c55472453530a366782e
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-17 09:58:10 +0400

    Updated wrapper div classname

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link 		https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * @since 	    1.1.0
+ * @since 	    1.2.0
  * @package 	conj-lite
  * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
  */
@@ -20,16 +20,17 @@
 <body <?php body_class(); ?>>
 	
 <?php
+wp_body_open();
+
 /**
  * Functions hooked into conj_lite_before_site action
  *
- * @hooked 	conj_lite_handheld_navigation 	- 10
+ * @hooked 	conj_lite_handheld_navigation 		- 10
  */
  do_action( 'conj_lite_before_site' ); ?>
 
-<div id="conj-lite-site__wrapper">
+<div id="conj-lite-site-wrapper">
 	<div id="page" class="hfeed site c-offcanvas-content-wrap">
-		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'conj-lite' ); ?></a>
 		<header id="masthead" class="site-header" role="banner">
 			<div class="col-full">
 				<?php
@@ -43,7 +44,7 @@
 				 * @hooked conj_lite_primary_navigation     - 40
 				 */
 				do_action( 'conj_lite_header' ); ?>
-			</div><!-- .col-full -->
+			</div>
 		</header><!-- #masthead -->
 		<div id="content" class="site-content" tabindex="-1">
 			<div class="conj-lite-col__full">
@@ -51,7 +52,8 @@
 				/**
 				 * Functions hooked into conj_lite_header action
 				 *
-				 * @hooked 	woocommerce_breadcrumb 			- 10
-				 * @hooked 	conj_lite_wc_shop_messages 		- 15
+				 * @hooked 	conj_lite_site_container_wrapper		 - 5
+				 * @hooked 	woocommerce_breadcrumb 					 - 10
+				 * @hooked 	conj_lite_wc_shop_messages 				 - 15
 				 */
 				do_action( 'conj_lite_site_content_top' ); ?>
