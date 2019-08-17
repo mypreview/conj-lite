78cf380349148c233b2284ae8dc85b2d25f69b71
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-17 09:56:23 +0400

    Updated wrapper div classname

--- sidebar.php
+++ sidebar.php
@@ -3,18 +3,17 @@
  * The sidebar containing the main widget area
  *
  * @link 		https://developer.wordpress.org/themes/basics/template-files/#template-partials
- * @since 	    1.1.0
+ * @since 	    1.2.0
  * @package 	conj-lite
  * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
  */
 
 if ( ! is_active_sidebar( 'sidebar-1' ) || apply_filters( 'conj_lite_disable_sidebar', FALSE ) ) {
 	return;
-}
+} // End If Statement
 ?>
-
 <aside id="secondary" class="widget-area" role="complementary" itemscope="itemscope" itemtype="https://schema.org/WPSideBar">
-	<div class="conj-lite-secondary-widget-area__wrapper">
+	<div class="widget-area__wrapper">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 </aside><!-- #secondary -->
