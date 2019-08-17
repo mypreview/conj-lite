60ef3a20b2395573dd3662cf6297fb63c304b104
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-17 17:28:58 +0400

    Updated nonce name
    Updated upsell notice container ID

--- assets/admin/js/script.js
+++ assets/admin/js/script.js
@@ -1,6 +1,7 @@
 /**
  * WordPress admin enhancements, specific to theme's up-sell admin notice.
  *
+ * @since       1.2.0
  * @package     conj-lite
  * @author     	MyPreview (Github: @mahdiyazdani, @mypreview)
  */
@@ -14,12 +15,12 @@
 
 	$( function() {
 		// Dismiss up-sell banner notice on user click!
-		$( document ).on( 'click', '#conj-lite-upsell-notice .notice-dismiss', function() {
+		$( document ).on( 'click', '#conj-lite-upsell .notice-dismiss', function() {
 			$.ajax( {
 				type: 'POST',
 				url: ajaxurl,
 				data: { 
-					nonce: conj_lite_vars.dismiss_upsell_notice_nonce, 
+					_ajax_nonce: conj_lite_vars.dismiss_upsell_nonce, 
 					action: 'conj_lite_dismiss_upsell_notice' 
 				},
 				dataType: 'json'
