5e8d7ac1f66ce45d5ff1fdc6e0960bdcb0d8d0d3
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-17 09:57:44 +0400

    Updated `the_post_navigation` params

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * E.g., it puts together the home page when no home.php file exists.
  * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @since 	    1.1.0
+ * @since 	    1.2.0
  * @package 	conj-lite
  * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
  */
@@ -19,38 +19,33 @@ get_header(); ?>
 
 		<?php
 		if ( have_posts() ) :
-
 			if ( is_home() && ! is_front_page() ) : ?>
-			
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
-
 			<?php
 			endif;
 
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
-
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/content', get_post_format() );
-
 			endwhile; // End of the loop.
 
-			the_posts_pagination( apply_filters( 'mypreview_conj_lite_post_pagination_args', array(
-				'prev_text' => '<span class="screen-reader-text">' . esc_html__( 'Previous page', 'conj-lite' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . esc_html__( 'Next page', 'conj-lite' ) . '</span>',
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . esc_html__( 'Page', 'conj-lite' ) . ' </span>',
+			the_posts_pagination( apply_filters( 'conj_post_pagination_args', array(
+				/* translators: 1: Open span tag, 2: Close span tag. */
+				'prev_text' => sprintf( esc_html_x( '%1$sPrevious page%2$s', 'posts pagination', 'conj-lite' ), '<span class="screen-reader-text">', '</span>' ),
+				/* translators: 1: Open span tag, 2: Close span tag. */
+				'next_text' => sprintf( esc_html_x( '%1$sNext page%2$s', 'posts pagination', 'conj-lite' ), '<span class="screen-reader-text">', '</span>' ),
+				/* translators: 1: Open span tag, 2: Close span tag. */
+				'before_page_number' => sprintf( esc_html_x( '%1$sPage%2$s', 'posts pagination label', 'conj-lite' ), '<span class="meta-nav screen-reader-text">', ' </span>' )
 			) ) );
-
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
-
 		endif; ?>
 
 		</main><!-- #main -->
