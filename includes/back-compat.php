4815c803538e0bb21daaeb4d41e29a995f84caa2
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-17 12:12:35 +0400

    Updated admin notice content

--- includes/back-compat.php
+++ includes/back-compat.php
@@ -3,12 +3,12 @@
  * Back compat functionality
  * Inspired by Twenty Sixteen `back-compat.php`
  *
- * Prevents Conj from running on WordPress versions prior to 5.0 or PHP versions prior to 7.0,
- * since this theme is not meant to be backward compatible beyond that and
- * relies on many newer functions and markup changes introduced in 5.0 and PHP version 7.0.
+ * Prevents CONJ Lite from running on old WordPress versions since this theme is not meant
+ * to be backward compatible beyond that and relies on many newer functions and markup 
+ * changes introduced in 5.2 and PHP version 7.0.
  *
  * @link 		https://github.com/WordPress/twentysixteen
- * @since 	    1.1.0
+ * @since 	    1.2.0
  * @package 	conj-lite
  * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
  */
@@ -39,7 +39,7 @@ if ( version_compare( $GLOBALS['wp_version'], CONJ_LITE_WORDPRESS_VERSION, '<' )
  * @return void
  */
 function conj_lite_back_compat_php_admin_notice() {
-	printf( '<div class="notice notice-warning"><p>%s</p></div>', wp_kses_post( conj_lite_get_php_notice_text() ) ); // WPCS: XSS okay.
+	printf( '<div class="notice notice-warning"><p><span class="feather-alert-triangle"></span>%s</p></div>', wp_kses_post( conj_lite_get_php_notice_text() ) ); // WPCS: XSS okay.
 }
 
 /**
@@ -48,8 +48,8 @@ function conj_lite_back_compat_php_admin_notice() {
  * @return string
  */
 function conj_lite_get_php_notice_text() {
-	/* translators: 1: Anchor open tag, 2: Anchor close tag, 3: Required PHP version number, 4: Running PHP version number, 5: Anchor open tag, 6: Anchor close tag. */
-	$notice_text = sprintf( esc_html__( '%1$sConj Lite - eCommerce WordPress Theme%2$s requires at least PHP version %3$s. You are running version %4$s. Please %5$supgrade%6$s and try again.', 'conj-lite' ), '<a href="' . esc_url( CONJ_LITE_THEME_URI ) . '" target="_blank" rel="noopener noreferrer nofollow">', '</a>', esc_html( CONJ_LITE_PHP_VERSION ), esc_html( PHP_VERSION ), '<a href="https://wordpress.org/support/update-php/" target="_blank" rel="noopener noreferrer nofollow">', '</a>' );
+	/* translators: 1: Required PHP version number, 2: Running PHP version number, 3: Anchor open tag, 4: Anchor close tag. */
+	$notice_text = sprintf( esc_html_x( 'CONJ Lite theme requires at least PHP version %1$s. You are running version %2$s. Please %3$supgrade%4$s and try again.', 'admin notice', 'conj-lite' ), esc_html( CONJ_LITE_PHP_VERSION ), esc_html( PHP_VERSION ), '<a href="https://wordpress.org/support/update-php" target="_blank" rel="noopener noreferrer nofollow">', '</a>' );
 
 	return apply_filters( 'conj_lite_php_notice_text', $notice_text );
 }
@@ -60,7 +60,7 @@ function conj_lite_get_php_notice_text() {
  * @return void
  */
 function conj_lite_back_compat_wordpress_admin_notice() {
-	printf( '<div class="notice notice-warning"><p>%s</p></div>', wp_kses_post( conj_lite_get_wordpress_notice_text() ) ); // WPCS: XSS okay.
+	printf( '<div class="notice notice-warning"><p><span class="feather-alert-triangle"></span>%s</p></div>', wp_kses_post( conj_lite_get_wordpress_notice_text() ) ); // WPCS: XSS okay.
 }
 
 /**
@@ -69,8 +69,8 @@ function conj_lite_back_compat_wordpress_admin_notice() {
  * @return string
  */
 function conj_lite_get_wordpress_notice_text() {
-	/* translators: 1: Anchor open tag, 2: Anchor close tag, 3: Required WordPress version number, 4: Running WordPress version number. */
-	$notice_text = sprintf( esc_html__( '%1$sConj Lite - eCommerce WordPress Theme%2$s requires at least WordPress version %3$s. You are running version %4$s. Please upgrade and try again.', 'conj-lite' ), '<a href="' . esc_url( CONJ_LITE_THEME_URI ) . '" target="_blank" rel="noopener noreferrer nofollow">', '</a>', esc_html( CONJ_LITE_WORDPRESS_VERSION ), $GLOBALS['wp_version'] );
+	/* translators: 1: Required PHP version number, 2: Running PHP version number, 3: Anchor open tag, 4: Anchor close tag. */
+	$notice_text = sprintf( esc_html_x( 'CONJ Lite theme requires at least WordPress version %1$s. You are running version %2$s. Please %3$supgrade%4$s and try again.', 'admin notice', 'conj-lite' ), esc_html( CONJ_LITE_WORDPRESS_VERSION ), $GLOBALS['wp_version'], '<a href="https://wordpress.org/download" target="_blank" rel="noopener noreferrer nofollow">', '</a>' );
 
 	return apply_filters( 'conj_lite_wordpress_notice_text', $notice_text );
 }
