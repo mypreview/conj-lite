0ac45d2115edb3992ef205b0cbf8613b3a3a7a27
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-08-20 17:12:44 +0400

    Updated site content hook names

--- includes/conj-lite-template-hooks.php
+++ includes/conj-lite-template-hooks.php
@@ -12,7 +12,7 @@
  *
  * @see  	conj_lite_handheld_navigation()
  */
-add_action( 'conj_lite_before_site', 		'conj_lite_handheld_navigation',     		  10 );
+add_action( 'conj_lite_before_site', 			'conj_lite_handheld_navigation',     	   10 );
 
 /**
  * Header
@@ -21,9 +21,9 @@ add_action( 'conj_lite_before_site', 		'conj_lite_handheld_navigation',     		  
  * @see  	conj_lite_site_branding()
  * @see  	conj_lite_primary_navigation()
  */
-add_action( 'conj_lite_header', 			'conj_lite_skip_links',                         0 );
-add_action( 'conj_lite_header', 			'conj_lite_site_branding',                     10 );
-add_action( 'conj_lite_header', 			'conj_lite_primary_navigation',                40 );
+add_action( 'conj_lite_header', 				'conj_lite_skip_links',                     0 );
+add_action( 'conj_lite_header', 				'conj_lite_site_branding',                 10 );
+add_action( 'conj_lite_header', 				'conj_lite_primary_navigation',            40 );
 
 /**
  * Site container
@@ -31,8 +31,8 @@ add_action( 'conj_lite_header', 			'conj_lite_primary_navigation',              
  * @see  conj_site_container_wrapper()
  * @see  conj_site_container_wrapper_close()
  */
-add_action( 'conj_site_content_top', 	 	'conj_lite_site_container_wrapper', 			5 );
-add_action( 'conj_site_content_bottom',		'conj_lite_site_container_wrapper_close', 	 	5 );
+add_action( 'conj_lite_site_content_top', 		'conj_lite_site_container_wrapper',		    5 );
+add_action( 'conj_lite_site_content_bottom',	'conj_lite_site_container_wrapper_close',	5 );
 
 /**
  * Footer
@@ -40,5 +40,5 @@ add_action( 'conj_site_content_bottom',		'conj_lite_site_container_wrapper_close
  * @see  	conj_lite_footer_widgets()
  * @see  	conj_lite_credit()
  */
-add_action( 'conj_lite_footer', 			 'conj_lite_footer_widgets',			  	   10 );
-add_action( 'conj_lite_footer', 			 'conj_lite_credit',					  	   20 );
+add_action( 'conj_lite_footer', 			 	'conj_lite_footer_widgets',			  	   10 );
+add_action( 'conj_lite_footer', 			 	'conj_lite_credit', 			 		   20 );
